96e62f1120a70d26ea905fd0b489aae7cb633d7f
Albert <Chesh397@mail.ru>
2018-03-17 13:03:16 +0300

    dwad

--- c9build/package.c9.caniuse.js
+++ c9build/package.c9.caniuse.js
@@ -138,7 +138,7 @@ define("plugins/c9.caniuse/c9.caniuse",
         var css3 = require("./bar.less");
         var css4 = require(options.staticPrefix + "/bar.less");
         var css5 = require("style!https://tgjmjgj.github.io/c9.caniuse/c9build/bar.less");
-        var css6 = require("bar.less");
+        var css6 = require("style!bar.less");
 
         console.log("css is \n" + css);
         console.log("css1 is \n" + css1);
