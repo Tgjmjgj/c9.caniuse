1c11bf90d14c7955f7b87d21f236e6679aa75b00
Albert <Chesh397@mail.ru>
2018-03-17 16:48:45 +0300

    fesfefsffbhbg g

--- c9build/mode/caniuse.js
+++ c9build/mode/caniuse.js
@@ -1,63 +1,60 @@
-define("plugins/c9.caniuse/c9.caniuse.caniuse",
-    [
-        "./browsers",
-        "text!./data.json"
-    ],
-    function(browsers, data) {
-        "use strict";
-        
-        data = JSON.parse(data).data;
+define(function(require, exports, module) {
+    "use strict";
 
-        return main;
+    var browsers = require("./browsers");
+    console.log("browsers is " + browsers);
+    var data = require("text!./data.json");
+    console.log("data is " + data);
+    data = JSON.parse(data).data;
 
-        function main(feature) {
-            var info;
-            feature = feature.replace(/\W*/g, '');
-            info = data[feature];
-            if (!info) {
-                feature = feature && slowFind(feature);
-                info = feature && data[feature];
+    module.exports = function(feature) {
+        var info;
+        feature = feature.replace(/\W*/g, '');
+        info = data[feature];
+        if (!info) {
+            feature = feature && slowFind(feature);
+            info = feature && data[feature];
+        }
+        return info && getSupportVersions(info.stats) || {};
+    };
+
+    function slowFind(query) {
+        var categories, description, key, keywords, matcher, ref, ref1, results, title;
+        results = [];
+        ref = data;
+        for (key in ref) {
+            ref1 = ref[key], title =
+                ref1.title, description =
+                ref1.description, keywords =
+                ref1.keywords, categories =
+                ref1.categories;
+            matcher = (key + title + description + keywords + categories);
+            matcher = matcher.toLowerCase().replace(/\W*/g, '');
+            if (matcher.match(query)) {
+                results.push(key);
             }
-            return info && getSupportVersions(info.stats) || {};
-        };
-
-        function slowFind(query) {
-            var categories, description, key, keywords, matcher, ref, ref1, results, title;
-            results = [];
-            ref = data;
-            for (key in ref) {
-                ref1 = ref[key], title =
-                    ref1.title, description =
-                    ref1.description, keywords =
-                    ref1.keywords, categories =
-                    ref1.categories;
-                matcher = (key + title + description + keywords + categories);
-                matcher = matcher.toLowerCase().replace(/\W*/g, '');
-                if (matcher.match(query)) {
-                    results.push(key);
-                }
-            }
-            return results[0];
         }
+        return results[0];
+    }
 
-        // TODO: Buggy, improve
-        function getMinimumSupportedVersion(browserVersions) {
-            var minVersion = null;
-            Object.keys(browserVersions).forEach(function(version) {
-                if (browserVersions[version] === "y" && !minVersion)
-                    minVersion = version;
-            });
-            return minVersion;
-        }
+    // TODO: Buggy, improve
+    function getMinimumSupportedVersion(browserVersions) {
+        var minVersion = null;
+        Object.keys(browserVersions).forEach(function(version) {
+            if (browserVersions[version] === "y" && !minVersion)
+                minVersion = version;
+        });
+        return minVersion;
+    }
 
-        function getSupportVersions(stats) {
-            var report = {};
-            Object.keys(stats).forEach(function(browser) {
-                var browserVersions = stats[browser];
-                var version = getMinimumSupportedVersion(browserVersions);
-                if (browsers.indexOf(browser) > -1)
-                    report[browser] = version;
-            });
-            return report;
-        }
+    function getSupportVersions(stats) {
+        var report = {};
+        Object.keys(stats).forEach(function(browser) {
+            var browserVersions = stats[browser];
+            var version = getMinimumSupportedVersion(browserVersions);
+            if (browsers.indexOf(browser) > -1)
+                report[browser] = version;
+        });
+        return report;
+    }
 });
